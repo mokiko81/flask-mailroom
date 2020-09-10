f2f19b32f6cf64b379f7d43a33e0dfedcfccb9b0
Adam Strong <str.adam.81@gmail.com>
2020-09-10 13:23:01 -0700

    Changed main.py, base.jinja2 and added newdonation.jinja2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -16,6 +16,23 @@ def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
     
+@app.route('/newdonation', methods=['GET', 'POST'])
+def new_donation():
+    #donators = Donor.select()
+    if request.method == 'POST':
+        try:
+            name = Donor.select().where(Donor.name == request.form['name']).get()
+        except:
+            newdonor = Donor(name=request.form['name'])
+            newdonor.save()
+            name = Donor.select().where(Donor.name == request.form['name']).get()
+            
+        amount = request.form['number']
+        donation = Donation(donor=name, value=amount)
+        donation.save()
+        return redirect(url_for('all'))
+    else:
+        return render_template('newdonation.jinja2')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
